1e2d20843be60eb7c2e868e73433b7ae319d9fc4
MrKeke <alexgraf0701@gmail.com>
2023-01-24 10:33:38 +0300

    add -R sort func

--- src/sort.js
+++ src/sort.js
@@ -1,15 +1,16 @@
-import _ from 'lodash'
-export default function sortbyabc(obj) {
-    let sorted = {};
-    let keys = _.sortBy(Object.keys(obj));
+import _ from 'lodash';
 
-    for (let i = 0; i < keys.length; i++) {
-        let key = keys[i];
-        if (typeof obj[key] === "object" && obj[key] !== null) {
-            sorted[key] = sortbyabc(obj[key]);
-        } else {
-            sorted[key] = obj[key];
-        }
+export default function sortbyabc(obj) {
+  const sorted = {};
+  const keys = _.sortBy(Object.keys(obj));
+
+  for (let i = 0; i < keys.length; i += 1) {
+    const key = keys[i];
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      sorted[key] = sortbyabc(obj[key]);
+    } else {
+      sorted[key] = obj[key];
     }
-    return sorted;
+  }
+  return sorted;
 }
