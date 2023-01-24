c2812403495e92e3db8b800d85a7b9e7642969dc
MrKeke <alexgraf0701@gmail.com>
2023-01-24 10:33:00 +0300

    rework for test // add return

--- cli/cli.js
+++ cli/cli.js
@@ -2,20 +2,23 @@ import fs from 'fs';
 import sortkeybyABC from '../src/sort.js';
 
 function doDiff(file1, file2) {
-  console.log('{');
+  let diffreslt = '{\n';
+  // console.log('{');
   const json1 = sortkeybyABC(JSON.parse(fs.readFileSync(file1, 'utf8')));
   const json2 = sortkeybyABC(JSON.parse(fs.readFileSync(file2, 'utf8')));
   Object.keys(json1).forEach((key) => {
-    if (json1[key] === json2[key]) console.log(`  ${key}: ${json1[key]}`);
+    if (json1[key] === json2[key]) diffreslt +=`  ${key}: ${json1[key]}\n`;
     else {
-      console.log(`- ${key}: ${json1[key]}`);
-      if (json2[key]) console.log(`+ ${key}: ${json2[key]}`);
+      diffreslt += `- ${key}: ${json1[key]}\n`;
+      if (json2[key]) diffreslt += `+ ${key}: ${json2[key]}\n`;
     }
   });
   Object.keys(json2).forEach((key) => {
-    if (!json1.hasOwnProperty(key)) console.log(`+ ${key}: ${json2[key]}`);
+    if (!json1.hasOwnProperty(key)) diffreslt += `+ ${key}: ${json2[key]}\n`;
   });
-  console.log('}');
+  diffreslt += '}';
+  console.log(diffreslt)
+  return diffreslt
 }
 
 export default doDiff;
