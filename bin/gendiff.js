9106c4bb33322e87c737db706a775c4b5c56963b
MrKeke <alexgraf0701@gmail.com>
2023-03-23 12:20:40 +0300

    check

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,12 +5,12 @@ import genDiff from '../index.js';
 program
   .version('0.0.1')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format', 'stylish')
+  .option('-f, --format <type>', 'output format', 'stylish')
   // .option('-h, --help', 'output usage information')
   .arguments('<filepath1> <filepath2>')
   .action((pathOne, pathTwo) => {
-    const { format } = program;
-    const diff = genDiff(pathOne, pathTwo, format);
+    // const { format } = program;
+    const diff = genDiff(pathOne, pathTwo, program.opts().format);
     console.log(diff);
   });
 program.parse(process.argv);
